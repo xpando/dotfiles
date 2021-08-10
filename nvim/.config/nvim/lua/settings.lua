8e0b37ba9f495f3dc24df69a2a57277884fa9f47
David Findley <xpando@gmail.com>
2021-08-09 21:45:31 -0700

    Add neovim basic config

---
+++ nvim/.config/nvim/lua/settings.lua
@@ -0,0 +1,37 @@
+vim.g.colors_name = 'onedark'
+
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 2
+vim.bo.softtabstop = 2
+
+-- Show relative line numbering
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+--Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+--Decrease update time
+vim.o.updatetime = 250
+vim.wo.signcolumn = 'yes'
+
+--Remap space as leader key
+vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+--Set colorscheme (order is important here)
+vim.o.termguicolors = true
+vim.g.onedark_terminal_italics = 2
+vim.cmd [[colorscheme onedark]]
+
+--Set statusbar
+vim.g.lightline = {
+  colorscheme = 'onedark',
+  active = { left = { { 'mode', 'paste' }, { 'gitbranch', 'readonly', 'filename', 'modified' } } },
+  component_function = { gitbranch = 'fugitive#head' },
+}
+
+-- maintain undo history between sessions
+vim.cmd([[set undofile]])
