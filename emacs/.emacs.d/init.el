6a253f521b0f650dadfeb5368ea3893c1e55386d
David Findley <xpando@gmail.com>
2021-08-22 21:52:24 -0700

    Working on emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -20,8 +20,8 @@
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "http://orgmode.org/melpa")
-			 ("elpa" . "https://elpa.gnu.org/packages")))
+			 ("org"   . "http://orgmode.org/melpa")
+			 ("elpa"  . "https://elpa.gnu.org/packages")))
 
 (package-initialize)
 (unless package-archive-contents
@@ -33,35 +33,25 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Ivy
-(use-package ivy
-  :diminish ;; don't put ivy in the mode line
-  :bind (("C-s" . swiper)
-	 :map ivy-minibuffer-map
-	 ("TAB" . ivy-alt-done)
-	 ("C-l" . ivy-alt-done)
-	 ("C-j" . ivy-next-line)
-	 ("C-k" . ivy-previous-line)
-	 :map ivy-switch-buffer-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-l" . ivy-done)
-	 ("C-d" . ivy-switch-buffer-kill)
-	 :map ivy-reverse-i-search-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
+(column-number-mode)
+(global-display-line-numbers-mode t)
 
-(ivy-mode 1)
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+		shell-mode-hook
+		term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 (use-package all-the-icons)
 
 (use-package doom-themes
-  :init (load-theme 'doom-dracula t))
+  :init (load-theme 'doom-one t))
 
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1))
+(doom-themes-visual-bell-config)
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package which-key
   :init (which-key-mode)
@@ -69,8 +59,134 @@
   :config
   (setq which-key-idle-delay 1))
 
-(unless (package-installed-p 'elixir-mode)
-  (package-install 'elixir-mode))
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :init
+  (ivy-mode 1))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("M-x"     . counsel-M-x)
+         ("C-x b"   . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         ;; ("C-M-j" . counsel-switch-buffer)
+         ("C-M-l"   . counsel-imenu)
+         :map minibuffer-local-map
+         ("C-r"     . 'counsel-minibuffer-history)))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command]  . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key]      . helpful-key))
+
+;; Keybindings
+(use-package general
+  :config
+  (general-create-definer rune/leader-keys
+    :keymaps '(normal insert visual emacs)
+    ;:prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (rune/leader-keys
+   "t"   '(:ignore t :which-key "toggles")
+   "tt"  '(counsel-load-theme :which-key "choose theme")))
+
+;; VIM "evil" mode
+
+;; Disable evil mode for some modes
+;; (defun rune/evil-hook ()
+;;  (dolist (mode '(custom-mode
+;;		  eshell-mode
+;;		  git-rebase-mode
+;;		  erc-mode
+;;		  circe-server-mode
+;;		  circe-chat-mode
+;;		  circe-query-mode
+;;		  sauron-mode
+;;		  term-mode))
+;;    (add-to-list 'evil-emacs-state-modes mode)))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  ;(setq evil-want-C-u-scroll t)
+  ;(setq evil-want-C-i-jump nill)
+  ;:hook (evil-mode . rune/evil-hook)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timout 0)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(rune/leader-keys
+ "ts" '(hydra-text-scale/body :which-key "scale text"))
+
+
+; Projects
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/EmacsProjects")
+    (setq projectile-project-search-path '("~/EmacsProjects")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+;; Git integration
+(use-package magit)
+
+
+(use-package alchemist)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -78,8 +194,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-mode which-key elixir-mode use-package ivy doom-modeline command-log-mode)))
-
+   '(alchemist magit counsel-projectile projectile hydra evil-collection evil general which-key use-package rainbow-delimiters ivy-rich helpful doom-themes doom-modeline counsel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
