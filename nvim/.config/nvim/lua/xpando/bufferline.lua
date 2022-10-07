25639a98737fabce55a2d884b9c270734244053d
David Findley <xpando@gmail.com>
2022-10-06 21:33:55 -0700

    Cleanup

--- nvim/.config/nvim/lua/xpando/bufferline.lua
+++ nvim/.config/nvim/lua/xpando/bufferline.lua
@@ -1,28 +1,26 @@
 require('bufferline').setup {
     options = {
-        mode = "buffers", -- set to "tabs" to only show tabpages instead
+        mode = "buffers",
         numbers = false,
-        close_command = "bdelete! %d",       -- can be a string | function, see "Mouse actions"
-        right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-        left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
-        middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
-        color_icons = true,                  -- whether or not to add the filetype icon highlights
-        show_buffer_icons = true,            -- disable filetype icons for buffers
+        middle_mouse_command = nil,
+        color_icons = true,
+        show_buffer_icons = true,
         show_buffer_close_icons = false,
-        show_buffer_default_icon = true,     -- whether or not an unrecognised filetype should show a default icon
+        show_buffer_default_icon = true,
         show_close_icon = false,
         show_tab_indicators = true,
-        persist_buffer_sort = true,          -- whether or not custom sorted buffers should persist
+        persist_buffer_sort = true,
         separator_style = "slant",
         enforce_regular_tabs = true,
         always_show_bufferline = true,
         sort_by = 'insert_at_end',
-        offsets = {
-            {
-                filetype = "NvimTree",
-                text = "File Explorer",
-                text_align = "left"
-            }
-        },
+        max_name_length = 18,
+        max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+        tab_size = 18,
+        offsets = {{
+            filetype = "NvimTree",
+            text = "File Explorer",
+            text_align = "center"
+        }}
     }
 }
