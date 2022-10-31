bab205d8dfc9c4874100904baa721b4ada8e0a6d
David Findley <david.findley@blockfi.com>
2022-10-31 10:38:24 -0700

    Remove Mac crap from Wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -136,22 +136,5 @@ config = {
     }
   }
 }
-
-function os.capture(cmd, raw)
-  local f = assert(io.popen(cmd, 'r'))
-  local s = assert(f:read('*a'))
-  f:close()
-  if raw then return s end
-  s = string.gsub(s, '^%s+', '')
-  s = string.gsub(s, '%s+$', '')
-  s = string.gsub(s, '[\n\r]+', ' ')
-  return s
-end
-
--- On MacOS when launching the app from Alfred the default program is "sh". 
--- I want to use zsh.
-if string.find(os.capture("uname", true), "Darwin") then
-  config["default_prog"] = {"/usr/local/bin/zsh", "-l"}
-end
   
 return config
