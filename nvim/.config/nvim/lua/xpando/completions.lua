e305d2780f9230ab4df2be989966ea657b4cfae4
David Findley <xpando@gmail.com>
2023-08-27 17:55:42 -0700

    wezterm and nvim config

--- nvim/.config/nvim/lua/xpando/completions.lua
+++
@@ -1,4 +0,0 @@
-local cmp = require 'cmp'
-
-cmp.setup {
-}
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,11 +1,11 @@
 local wt = require 'wezterm';
 
 -- This config uses the following fonts:
--- 
+--
 -- Iosevka
 -- Nerd Font Symbols Only
 -- Noto Color Emoji
--- 
+--
 -- Install on MacOS with:
 -- brew install --cask font-iosevka
 -- brew install --cask font-symbols-only-nerd-font
@@ -14,7 +14,7 @@ local wt = require 'wezterm';
 config = {
   enable_wayland = true,
 
-  color_scheme = "DaveDark",
+  color_scheme = 'DaveDark',
 
   default_cursor_style = 'SteadyUnderline',
 
@@ -24,20 +24,31 @@ config = {
   -- clean_exit_codes = {127},
 
   keys = {
-    { key = 'UpArrow', mods = 'SHIFT', action = wt.action.ScrollToPrompt(-1) },
+    { key = 'UpArrow',   mods = 'SHIFT', action = wt.action.ScrollToPrompt(-1) },
     { key = 'DownArrow', mods = 'SHIFT', action = wt.action.ScrollToPrompt(1) },
-  },  
+  },
 
   font_size = 16.0,
-  font = wt.font_with_fallback({
-    { family="Iosevka", weight="ExtraLight" },
-    { family="Symbols Nerd Font Mono" },
-    { family="Noto Color Emoji" },
-    { family="JoyPixels" },
-  }),
+  font = wt.font_with_fallback {
+    { family = "IosevkaTerm Nerd Font Propo", weight = "ExtraLight", },
+  },
+  font_rules = {
+    {
+      intensity = "Bold",
+      font = wt.font_with_fallback({
+        { family="IosevkaTerm Nerd Font Propo", weight="Light" }
+      })
+    },
+    {
+      intensity = "Half",
+      font = wt.font_with_fallback({
+        { family="IosevkaTerm Nerd Font Propo", weight="ExtraLight" }
+      })
+    }
+  },
   freetype_load_target = "HorizontalLcd",
-  bold_brightens_ansi_colors = true,
-  window_background_opacity = 0.95,
+  bold_brightens_ansi_colors = false,
+  window_background_opacity = 1.0,
   text_background_opacity = 1.0,
 
   -- set to false to disable the tab bar completely
@@ -47,80 +58,82 @@ config = {
   -- a single tab in the window
   hide_tab_bar_if_only_one_tab = true,
 
-  colors = {
-    tab_bar = {
+  use_fancy_tab_bar = true,
 
-      -- The color of the strip that goes along the top of the window
-      -- background = "#0b0022",
-      background = "#1e1e1e",
+  -- colors = {
+  --   tab_bar = {
 
-      -- The active tab is the one that has focus in the window
-      active_tab = {
-        -- The color of the background area for the tab
-        bg_color = "#1e1e1e",
-        -- The color of the text for the tab
-        fg_color = "#84f542",
+  --     -- The color of the strip that goes along the top of the window
+  --     -- background = "#0b0022",
+  --     background = "#1e1e1e",
 
-        -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
-        -- label shown for this tab.
-        -- The default is "Normal"
-        intensity = "Normal",
+  --     -- The active tab is the one that has focus in the window
+  --     active_tab = {
+  --       -- The color of the background area for the tab
+  --       bg_color = "#1e1e1e",
+  --       -- The color of the text for the tab
+  --       fg_color = "#84f542",
 
-        -- Specify whether you want "None", "Single" or "Double" underline for
-        -- label shown for this tab.
-        -- The default is "None"
-        underline = "None",
+  --       -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+  --       -- label shown for this tab.
+  --       -- The default is "Normal"
+  --       intensity = "Normal",
 
-        -- Specify whether you want the text to be italic (true) or not (false)
-        -- for this tab.  The default is false.
-        italic = false,
+  --       -- Specify whether you want "None", "Single" or "Double" underline for
+  --       -- label shown for this tab.
+  --       -- The default is "None"
+  --       underline = "None",
 
-        -- Specify whether you want the text to be rendered with strikethrough (true)
-        -- or not for this tab.  The default is false.
-        strikethrough = false,
-      },
+  --       -- Specify whether you want the text to be italic (true) or not (false)
+  --       -- for this tab.  The default is false.
+  --       italic = false,
 
-      -- Inactive tabs are the tabs that do not have focus
-      inactive_tab = {
-        bg_color = "#000000",
-        fg_color = "#c0c0c0",
+  --       -- Specify whether you want the text to be rendered with strikethrough (true)
+  --       -- or not for this tab.  The default is false.
+  --       strikethrough = false,
+  --     },
 
-        -- The same options that were listed under the `active_tab` section above
-        -- can also be used for `inactive_tab`.
-      },
+  --     -- Inactive tabs are the tabs that do not have focus
+  --     inactive_tab = {
+  --       bg_color = "#000000",
+  --       fg_color = "#c0c0c0",
 
-      -- You can configure some alternate styling when the mouse pointer
-      -- moves over inactive tabs
-      inactive_tab_hover = {
-        bg_color = "#3b3052",
-        fg_color = "#909090",
-        italic = true,
+  --       -- The same options that were listed under the `active_tab` section above
+  --       -- can also be used for `inactive_tab`.
+  --     },
 
-        -- The same options that were listed under the `active_tab` section above
-        -- can also be used for `inactive_tab_hover`.
-      },
+  --     -- You can configure some alternate styling when the mouse pointer
+  --     -- moves over inactive tabs
+  --     inactive_tab_hover = {
+  --       bg_color = "#3b3052",
+  --       fg_color = "#909090",
+  --       italic = true,
 
-      -- The new tab button that let you create new tabs
-      new_tab = {
-        bg_color = "#1b1032",
-        fg_color = "#808080",
+  --       -- The same options that were listed under the `active_tab` section above
+  --       -- can also be used for `inactive_tab_hover`.
+  --     },
 
-        -- The same options that were listed under the `active_tab` section above
-        -- can also be used for `new_tab`.
-      },
+  --     -- The new tab button that let you create new tabs
+  --     new_tab = {
+  --       bg_color = "#1b1032",
+  --       fg_color = "#808080",
 
-      -- You can configure some alternate styling when the mouse pointer
-      -- moves over the new tab button
-      new_tab_hover = {
-        bg_color = "#3b3052",
-        fg_color = "#909090",
-        italic = true,
+  --       -- The same options that were listed under the `active_tab` section above
+  --       -- can also be used for `new_tab`.
+  --     },
 
-        -- The same options that were listed under the `active_tab` section above
-        -- can also be used for `new_tab_hover`.
-      }
-    }
-  }
+  --     -- You can configure some alternate styling when the mouse pointer
+  --     -- moves over the new tab button
+  --     new_tab_hover = {
+  --       bg_color = "#3b3052",
+  --       fg_color = "#909090",
+  --       italic = true,
+
+  --       -- The same options that were listed under the `active_tab` section above
+  --       -- can also be used for `new_tab_hover`.
+  --     }
+  --   }
+  -- }
 }
-  
+
 return config
