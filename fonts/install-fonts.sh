6ea496b3b21357519f949f2e0a77c94677a9b5c2
David Findley <xpando@gmail.com>
2024-08-11 03:18:18 -0700

    fonts

---
+++ fonts/install-fonts.sh
@@ -0,0 +1,10 @@
+set -e
+
+FONT_DIR=~/.local/share/fonts
+
+go run gh-download.go --owner ryanoasis --repo nerd-fonts --asset 'NerdFontsSymbolsOnly\.zip'
+unzip '/tmp/NerdFontsSymbolsOnly.zip' '*.ttf' -d "$FONT_DIR"
+
+go run gh-download.go --owner be5invis --repo Iosevka --asset '^PkgTTC-Iosevka-\d+\.\d+\.\d+\.zip$'
+unzip '/tmp/PkgTTC-Iosevka-*.zip' '*.ttc' -d "$FONT_DIR"
+
